03ade74fba5902b10441da604f4feab0b2b88b2a
potabattiram <ram.potabatti@neutrinos.co>
2023-07-27 10:52:38 +0530

    Built Search API

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000; 
+const cors = require('cors');
 
+app.use(cors({
+  origin: "*"
+}))
 app.post('/on_search', (req, res) => {
     console.log(res);
     res.send('success');
